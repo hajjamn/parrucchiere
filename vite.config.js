4130c855a38963840dd252a4d55d8fd87b4e4f5f
hajjamn <hajjamn@gmail.com>
2025-05-01 17:37:27 +0200

    FIX: config

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ const path = require('path') // <-- require path from node
 export default defineConfig({
     plugins: [
         laravel({
+            base: '/dreamscenter/',
             // edit the first value of the array input to point to our new sass files and folder.
             input: ['resources/scss/app.scss', 'resources/js/app.js'],
             refresh: true,
