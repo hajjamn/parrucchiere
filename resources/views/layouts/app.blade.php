243ce3c933e415662e653681be2048c0a2174db5
HajjamN <hajjamn@gmail.com>
2025-05-20 17:30:33 +0200

    FIX: route logo url

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,9 +35,12 @@
 
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand d-flex align-items-center" href="{{ url('/') }}">
+                <a class="navbar-brand d-flex align-items-center" href="{{ route('admin.service-logs.index') }}">
+
                     <div class="">
-                        <a class="navbar-brand d-flex align-items-center" href="{{ url('/') }}">
+                        <a class="navbar-brand d-flex align-items-center"
+                            href="{{ route('admin.service-logs.index') }}">
+
                             <img src="{{ asset('images/favicon.png') }}" alt="Logo" style="height: 40px;">
                         </a>
 
