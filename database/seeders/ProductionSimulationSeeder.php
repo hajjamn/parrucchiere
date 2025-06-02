5330b0e130f5e53b8f672a555c70b7130d27a6ce
HajjamN <hajjamn@gmail.com>
2025-06-02 13:20:20 +0200

    FIX: migration per fixare

--- database/seeders/ProductionSimulationSeeder.php
+++ database/seeders/ProductionSimulationSeeder.php
@@ -21,7 +21,11 @@ class ProductionSimulationSeeder extends Seeder
     }
 }
 
-/* php artisan migrate --path=database/migrations/2014_10_12_000000_create_users_table.php
+/*
+php artisan migrate:fresh
+php artisan migrate:reset
+
+php artisan migrate --path=database/migrations/2014_10_12_000000_create_users_table.php
 php artisan migrate --path=database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
 php artisan migrate --path=database/migrations/2019_08_19_000000_create_failed_jobs_table.php
 php artisan migrate --path=database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -30,4 +34,10 @@ php artisan migrate --path=database/migrations/2025_04_02_093033_create_services
 php artisan migrate --path=database/migrations/2025_04_22_070104_create_service_logs_table.php
 
 php artisan db:seed --class=ProductionSimulationSeeder
+
+php artisan migrate --path=database/migrations/2025_06_01_132921_add_commission_percentage_to_service_logs_table.php
+php artisan migrate --path=database/migrations/2025_06_01_142915_add_uses_quantity_to_services_table.php
+
+php artisan migrate --path=database/migrations/2025_06_02_120106_fix_existing_data.php
+
  */
